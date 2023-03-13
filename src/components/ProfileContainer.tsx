1ee243f1215c66336fb026ce822c86a893d885b9
claudiocoder <clauartur98@gmail.com>
2023-03-12 19:36:54 -0600

    :sparkles: - Add Profile Image

---
+++ src/components/ProfileContainer.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+import profileImage from '../assets/profile.jpg'
+import { roboto, inter } from '@/utils/fonts'
+export const ProfileContainer = () => {
+    return (
+        <>
+            <div className="flex w-full h-screen flex-col items-center justify-center">
+                <Image
+                    className="rounded-full drop-shadow-xl m-3 border-2 border-gray-200"
+                    src={profileImage}
+                    title="Claudio Coder Profile"
+                    alt="ProfileImage"
+                    width={200}
+                />
+                <h1 className={`${roboto.className} text-xl font-bold`}>
+                    Hi, my name is Claudio!
+                </h1>
+                <h2 className={`${inter.className} text-base`}>
+                    Software Developer
+                </h2>
+            </div>
+        </>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    theme: {},
+    plugins: [],
 }
