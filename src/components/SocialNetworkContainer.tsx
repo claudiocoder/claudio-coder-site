7e6095a0c51090bd143f382c08bc797f51516ecb
claudiocoder <clauartur98@gmail.com>
2023-03-12 22:08:43 -0600

    :sparkles: - Add Social Network

---
+++ src/components/SocialNetworkContainer.tsx
@@ -0,0 +1,41 @@
+import {
+    faGithub,
+    faLinkedin,
+    faSquareTwitter,
+    faTwitch,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+
+const SocialNetwork = () => {
+    return (
+        <div className="flex items-center justify-around w-full max-w-[200px]">
+            <Link href="https://twitter.com/claudio_coder" target="_blank">
+                <FontAwesomeIcon
+                    className="text-3xl text-blue-500 cursor-pointer"
+                    icon={faSquareTwitter}
+                />
+            </Link>
+            <Link href="https://github.com/claudiocoder" target="_blank">
+                <FontAwesomeIcon
+                    className="text-3xl text-gray-800 cursor-pointer"
+                    icon={faGithub}
+                />
+            </Link>
+            <Link href="https://www.linkedin.com/in/cmorenomx" target="_blank">
+                <FontAwesomeIcon
+                    className="text-3xl text-blue-800 cursor-pointer"
+                    icon={faLinkedin}
+                />
+            </Link>
+            <Link href="https://www.twitch.tv/claudio_coder" target="_blank">
+                <FontAwesomeIcon
+                    className="text-3xl text-purple-800 cursor-pointer"
+                    icon={faTwitch}
+                />
+            </Link>
+        </div>
+    )
+}
+
+export default SocialNetwork
