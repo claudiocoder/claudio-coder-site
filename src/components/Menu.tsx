610f150b1233398e43e3c4f629f59f49b1248395
claudiocoder <clauartur98@gmail.com>
2023-04-09 17:48:40 -0600

    :sparkles: - Add Layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 import styles from '../styles/Menu.module.css'
-import { roboto } from '@/utils/fonts'
 
 const Menu = () => {
     const router = useRouter()
