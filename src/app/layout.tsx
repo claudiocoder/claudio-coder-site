cc270b8821f64c901419d5a5e318f3aab356150e
claudiocoder <clauartur98@gmail.com>
2023-12-13 00:00:13 -0600

    :sparkles: - Add Google Analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import '@/styles/globals.css'
 import ProfileContainer from '@/components/ProfileContainer'
-import GoogleAnalytics from '@/components/Googleanalytics'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 config.autoAddCss = false
 
 export default function RootLayout({
