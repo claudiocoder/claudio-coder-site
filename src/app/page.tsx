39cf4ca3f5463a1cc91c746cdd57d0fb9a712ea2
Claudio Moreno <clauartur98@gmail.com>
2023-12-17 00:59:13 -0600

    :memo: - Improve content blog 01 (#16)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from "next";
 import { allBlogs } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
-import Head from "next/head";
 import PostCard from "@/components/PostCard";
 
 export const metadata: Metadata = {
@@ -19,12 +18,6 @@ export default function Home() {
 
   return (
     <>
-      <Head>
-        <title>Claudio&#180;s Page</title>
-        <meta name="description" content="Claudio Coder Personal Page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="sm:m-8">
         <h1 className="mb-6 sm:mb-8 text-3xl font-black">Blog</h1>
         <div className="flex flex-wrap gap-5">
