1f44407b2dfa1a121d67f09563f5c58359a0b672
claudiocoder <clauartur98@gmail.com>
2025-02-13 14:22:19 -0600

    :sparkles: - Update linting tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const { fontFamily } = require('tailwindcss/defaultTheme')
-
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {},
