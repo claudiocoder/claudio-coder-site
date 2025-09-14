d35b0027f1a5aa46f6d32297be7af39a192c5f1c
Claudio Moreno <clauartur98@gmail.com>
2025-09-14 15:49:37 -0600

    :sparkles: - Improve Card styles

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,30 +6,32 @@ import { Blog } from '@/lib/models/Blog'
 
 const PostCard = (post: Blog) => {
   return (
-    <div className='w-[100%] md:w-[80%] lg:w-[60%] shadow-[0_5px_15px_rgba(0,0,0,0.35)] rounded-md flex items-center bg-slate-50 rounded-l-lg max-h-[180px]'>
-      <div className='rounded-l-lg'>
-        <Image
-          width={180}
-          height={150}
-          src={post.thumbnailUrl as string}
-          alt={post.title as string}
-          className='rounded-l-lg'
-        />
+    <Link href={post.slug as string}>
+      <div className='w-[100%] lg:w-[350px] shadow-[0_5px_15px_rgba(0,0,0,0.35)] rounded-md flex flex-col items-center bg-slate-50 rounded-l-lg'>
+        <div className='rounded-lg'>
+          <Image
+            width={350}
+            height={350}
+            src={post.thumbnailUrl as string}
+            alt={post.title as string}
+            className='rounded-md object-cover max-h-[300px] '
+          />
+        </div>
+        <div className='flex flex-col items-start justify-between px-3 w-full h-full'>
+          <h2
+            className={`${_roboto.className} my-5 text-base sm:text-2xl font-bold w-full text-left`}
+          >
+            {post.title as string}
+          </h2>
+          <time
+            dateTime={post.date as string}
+            className='mb-2 block text-xs w-full text-gray-600 text-right'
+          >
+            {format(parseISO(post.date as string), 'MMMM dd, yyyy')}
+          </time>
+        </div>
       </div>
-      <div className='flex flex-col items-start justify-between px-3 w-full h-full'>
-        <h2
-          className={`${_roboto.className} my-5 text-base sm:text-xl font-bold w-full text-left`}
-        >
-          <Link href={post.slug as string}>{post.title as string}</Link>
-        </h2>
-        <time
-          dateTime={post.date as string}
-          className='mb-2 block text-xs w-full text-gray-600 text-right'
-        >
-          {format(parseISO(post.date as string), 'MMMM dd, yyyy')}
-        </time>
-      </div>
-    </div>
+    </Link>
   )
 }
 
