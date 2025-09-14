30f52d9d87dc3eabe3e290e27f532c1c259f524c
Claudio Moreno <clauartur98@gmail.com>
2025-09-14 15:35:25 -0600

    :sparkles: - Remove legacyBehavior social media links

--- src/components/SocialNetworkContainer.tsx
+++ src/components/SocialNetworkContainer.tsx
@@ -10,41 +10,25 @@ import Link from 'next/link'
 const SocialNetwork = () => {
   return (
     <div className='flex items-center justify-around w-full'>
-      <Link
-        href='https://twitter.com/claudio_coder'
-        target='_blank'
-        legacyBehavior
-      >
+      <Link href='https://twitter.com/claudio_coder' target='_blank'>
         <FontAwesomeIcon
           className='text-3xl text-black-500 cursor-pointer'
           icon={faXTwitter}
         />
       </Link>
-      <Link
-        href='https://github.com/claudiocoder'
-        target='_blank'
-        legacyBehavior
-      >
+      <Link href='https://github.com/claudiocoder' target='_blank'>
         <FontAwesomeIcon
           className='text-3xl text-gray-800 cursor-pointer'
           icon={faGithub}
         />
       </Link>
-      <Link
-        href='https://www.linkedin.com/in/cmorenomx'
-        target='_blank'
-        legacyBehavior
-      >
+      <Link href='https://www.linkedin.com/in/cmorenomx' target='_blank'>
         <FontAwesomeIcon
           className='text-3xl text-blue-800 cursor-pointer'
           icon={faLinkedin}
         />
       </Link>
-      <Link
-        href='https://www.twitch.tv/claudio_coder'
-        target='_blank'
-        legacyBehavior
-      >
+      <Link href='https://www.twitch.tv/claudio_coder' target='_blank'>
         <FontAwesomeIcon
           className='text-3xl text-purple-800 cursor-pointer'
           icon={faTwitch}
