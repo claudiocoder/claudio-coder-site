e279ff2cf09c62d943241825fd90fdeec9071e74
Claudio Moreno <clauartur98@gmail.com>
2025-09-14 15:57:00 -0600

    :sparkles: - Improve Card styles

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,14 +7,14 @@ import { Blog } from '@/lib/models/Blog'
 const PostCard = (post: Blog) => {
   return (
     <Link href={post.slug as string}>
-      <div className='w-[100%] lg:w-[350px] shadow-[0_5px_15px_rgba(0,0,0,0.35)] rounded-md flex flex-col items-center bg-slate-50 rounded-l-lg'>
+      <div className='w-[100%] lg:w-[350px] shadow-[0_5px_15px_rgba(0,0,0,0.35)] rounded-md flex flex-col items-center bg-slate-50'>
         <div className='rounded-lg'>
           <Image
             width={350}
             height={350}
             src={post.thumbnailUrl as string}
             alt={post.title as string}
-            className='rounded-md object-cover max-h-[300px] '
+            className='rounded-md object-cover max-h-[300px]'
           />
         </div>
         <div className='flex flex-col items-start justify-between px-3 w-full h-full'>
